3d13a4dc66ad66ab350fab8d918922b3aea69098
Mateusz Matyska <mateusz.matyska_extern@bsdex.de>
2022-09-27 22:13:27 +0200

    feat: add redux with persistor

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import store from './src/store/Store';
+import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import AppStack from './src/navigation/stacks/AppStack';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
 
 const App: React.FC = () => {
+  let persistor = persistStore(store);
+
   return (
-    <NavigationContainer>
-      <AppStack />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <AppStack />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- src/screens/loginScreen/LoginScreen.tsx
+++ src/screens/loginScreen/LoginScreen.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, Text, View, Button} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {getValue} from '../../store/Slices/CounterSlice';
+import {increment, decrement} from '../../store/Slices/CounterSlice';
 
 const LoginScreen: React.FC = () => {
+  const value = useSelector(getValue);
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView>
       <View>
         <Text>React Native App Login</Text>
+        <Text>Value {value}</Text>
+        <Button
+          onPress={() => {
+            dispatch(increment());
+          }}
+          title="Add"
+        />
+        <Button
+          onPress={() => {
+            dispatch(decrement());
+          }}
+          title="Remove"
+        />
       </View>
     </SafeAreaView>
   );
